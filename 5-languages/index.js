1a23dbfd6dfac0e763f4dec2a54cd64fe0dafaa3
JuliaKrasnoperva <julia.krasnoperova97@gmail.com>
2023-12-17 16:29:36 +0500

    добавлена папка 5-languages

---
+++ 5-languages/index.js
@@ -0,0 +1,15 @@
+let language = "czsss";
+
+switch (language) {
+  case "ru":
+    console.log("Доброе утро!");
+    break;
+  case "cz":
+    console.log("Dobré ráno!");
+    break;
+  case "en":
+    console.log("Good morning!");
+    break;
+  default:
+    console.log("Не верно заполнено поле!");
+}
